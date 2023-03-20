1dcd1e0279789dbbf0f717492da5ded40601ac77
João Henrique Silva <joaohsm13@gmail.com>
2023-03-20 19:05:50 -0300

    Create CSTREET.cpp

---
+++ SPOJ/CSTREET/CSTREET.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+
+using namespace std;
+
+vector<pair<int,pair<int,int>>>adj;
+vector<int>pai(1000000),r(1000000);
+int caminho;
+
+
+
+void makeSet(int n){
+	adj.clear();
+	caminho=0;
+    for(int i=0; i<=n; i++){
+        pai[i]=i;
+        r[i]=1;
+    }
+}
+
+
+
+int find(int x){
+    if(pai[x]==x){
+        return x;
+    }
+    return pai[x]=find(pai[x]);
+}
+
+
+bool sameSet(int x, int y){
+    return find(x)==find(y);
+}
+
+
+
+void unite(int x, int y){
+    x=find(x);
+    y=find(y);
+    if(r[x]>r[y]){
+        pai[y]=x;
+        r[x]+=r[y];
+    }
+    else{
+        pai[x]=y;
+        r[y]+=r[x];
+    }
+}
+
+
+
+void kruskal(int n){
+    for(int i=0; i<n; i++){
+        int peso=adj[i].f;
+        int a=adj[i].s.f,b=adj[i].s.s;
+       
+        if(!sameSet(a,b)){
+            unite(a,b);
+            caminho+=peso;
+        }  
+    }
+}
+
+
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+   	//~ freopen("input.txt","r",stdin);
+	//~ freopen("output.txt","w",stdout);
+	int n,e,t,x,y,z,p;
+	cin>>t;
+	while(t--){
+		cin>>p;
+		cin>>n>>e;
+		makeSet(n);
+		for(int j=0;j<e;j++){
+			cin>>x>>y>>z;
+			adj.push_back({z,{x,y}});
+		}
+		sort(adj.begin(), adj.end());
+		kruskal(adj.size());
+		cout<<caminho*p<<endl;
+	}
+}	
