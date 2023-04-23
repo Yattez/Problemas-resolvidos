4ffb3bcd870e1dd621004f51db4f9cc12028e44d
João Henrique Silva <joaohsm13@gmail.com>
2023-04-23 16:37:48 -0300

    Create 686.cpp

---
+++ UVA/686/686.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define endl '\n'
+#define int long long
+
+using namespace std;
+const int MAX = 3300000;
+ 
+vector <int> primes;
+ 
+void sieveSundaram(){
+    bool marked[MAX/2 + 100] = {0};
+    for (int i=1; i<=(sqrt(MAX)-1)/2; i++)
+        for (int j=(i*(i+1))<<1; j<=MAX/2; j=j+2*i+1)
+            marked[j] = true;
+    primes.push_back(2);
+    for (int i=1; i<=MAX/2; i++)
+        if (marked[i] == false)
+            primes.push_back(2*i + 1);
+}
+ 
+void findPrimes(int n){
+    int x=0;
+    for (int i=0 ; primes[i] <= n/2; i++){
+        int diff = n - primes[i];
+        if (binary_search(primes.begin(), primes.end(), diff)){
+           x++;
+        }
+    }
+    cout<<x<<endl;
+}
+signed main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+    //~ freopen("input.txt","r",stdin);
+	//~ freopen("output.txt","w",stdout);
+	sieveSundaram();
+	int n;
+	cin>>n;
+	while(n!=0){
+		findPrimes(n);
+		cin>>n;
+	}
+}
